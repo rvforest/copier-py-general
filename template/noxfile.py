ce4f29cadea501ce1e065ef2b258399a92ff09d6
rvforest@gmail.com <rvforest@gmail.com>
2025-04-16 23:07:23 -0400

    Add nox session for pytest

--- template/noxfile.py
+++ template/noxfile.py
@@ -36,10 +36,29 @@ def fix(session: nox.Session) -> None:
     session.run("ruff", "format")
 
 
+@nox.session(venv_backend="uv")
+def test(session: nox.Session) -> None:
+    """Run tests"""
+    _install_all_deps(session)
+    session.run("pytest")
+
+
 def _install_dev_deps(session: nox.Session) -> None:
+    _run_install(only_dev=True)
+
+
+def _install_all_deps(session: nox.Session) -> None:
+    _run_install(session, only_dev=False)
+
+
+def _run_install(session: nox.Session, only_dev: bool) -> None:
+    cmd = ["uv", "sync"]
+    if only_dev:
+        cmd.append("--only-dev")
     session.run_install(
         "uv",
         "sync",
         "--only-dev",
         env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
     )
+    
